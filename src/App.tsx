f30d57b76711bfb8d0ff1509a9ffc9fb865f7fad
Mjnari <mjnarir@gmail.com>
2022-12-06 23:33:26 -0500

    new: Add loading indicators when waiting on server

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from 'react';
 import { ApiClient, ToDo } from './ApiClient';
 import './App.css';
 
-const apiClient = new ApiClient(false);
+const apiClient = new ApiClient(true);
 
 function App() {
   const [todos, setTodos] = useState<ToDo[]>([]);
   const [label, setLabel] = useState('');
+  const [loadingList, setLoadingList] = useState(true);
 
   function handleGetToDos() {
     apiClient
       .getToDos()
-      .then((fetchedTodos) => setTodos(fetchedTodos))
+      .then((fetchedTodos) => {
+        setTodos(fetchedTodos)
+        setLoadingList(false);
+      })
       .catch(console.error);
   }
 
@@ -20,11 +24,13 @@ function App() {
   }, [setTodos]);
 
   async function handleAddToDoClick(newLabel: string) {
+    setLoadingList(true);
     await apiClient.addTodo(newLabel);
     handleGetToDos();
   }
 
   async function handleToggleDone(id: string) {
+    setLoadingList(true);
     await apiClient.toggleDone(id);
     handleGetToDos();
   }
@@ -39,21 +45,28 @@ function App() {
           onChange={(e) => setLabel(e.target.value)}
           placeholder="Buy groceries"
         />
-        <button onClick={() => handleAddToDoClick(label)}>Add ToDo</button>
+        {loadingList
+          ? <span>Loading...</span>
+          : <button onClick={() => handleAddToDoClick(label)}>Add ToDo</button>
+        }
       </div>
 
-      {todos.map((todo) => (
-        <div key={todo.id} className="todo-item">
-          <label
-            style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
-          >
-            {todo.label}
-          </label>
-          <button onClick={() => handleToggleDone(todo.id)}>
-            Mark {todo.done ? 'Undone' : 'Done'}
-          </button>
-        </div>
-      ))}
+      {loadingList
+        ? <p>Loading...</p>
+        : (
+          todos.map((todo) => (
+            <div key={todo.id} className="todo-item">
+              <label
+                style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
+              >
+                {todo.label}
+              </label>
+              <button onClick={() => handleToggleDone(todo.id)}>
+                Mark {todo.done ? 'Undone' : 'Done'}
+              </button>
+            </div>
+          )))
+      }
     </>
   );
 }
